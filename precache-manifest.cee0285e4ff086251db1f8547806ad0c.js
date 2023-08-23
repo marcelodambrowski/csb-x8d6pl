7ac7c4b5d425f04b1f7a17df476389c494185fa2
Code Sandbox <hello@codesandbox.io>
2023-08-23 14:34:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cee0285e4ff086251db1f8547806ad0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b146e5fc3f33db7dcb6a90ed2913965",
+    "url": "https://.github.io/csb-x8d6pl/index.html"
+  },
+  {
+    "revision": "5f57f1b0640248cc85af",
+    "url": "https://.github.io/csb-x8d6pl/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "9ceaa78ca620986ef2ae",
+    "url": "https://.github.io/csb-x8d6pl/static/js/2.a2af4772.chunk.js"
+  },
+  {
+    "revision": "5f57f1b0640248cc85af",
+    "url": "https://.github.io/csb-x8d6pl/static/js/main.9c362af5.chunk.js"
+  },
+  {
+    "revision": "a469a1c26d55251cf24a",
+    "url": "https://.github.io/csb-x8d6pl/static/js/runtime~main.d32eb8c1.js"
+  },
+  {
+    "revision": "c9047f9e9a6b9f76cd74974edf545f30",
+    "url": "https://.github.io/csb-x8d6pl/static/media/median_income.c9047f9e.kml"
+  }
+]);
